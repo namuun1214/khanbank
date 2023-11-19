c408d5ecb6f1579b5463406ce228d4bf371fa0d7
namuu1214 <namuu1214@gmail.com>
2023-11-19 21:02:59 +0800

    done

--- app/src/screens/ShakeScreen.tsx
+++ app/src/screens/ShakeScreen.tsx
@@ -14,19 +14,19 @@ import {
 import { useEffect } from 'react'
 import RNShake from 'react-native-shake'
 import RNShakeEvent from 'react-native-shake-event'
-import OpenAI from 'openai'
+// import OpenAI from 'openai'
 
-const openai = new OpenAI()
-async function main() {
-  console.log('OPenAI running')
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: 'system', content: 'You are a helpful assistant.' }],
-    model: 'gpt-3.5-turbo',
-  })
+// const openai = new OpenAI()
+// async function main() {
+//   console.log('OPenAI running')
+//   const completion = await openai.chat.completions.create({
+//     messages: [{ role: 'system', content: 'You are a helpful assistant.' }],
+//     model: 'gpt-3.5-turbo',
+//   })
 
-  console.log('here', completion.choices[0])
-  return completion.choices[0].message.content
-}
+//   console.log('here', completion.choices[0])
+//   return completion.choices[0].message.content
+// }
 export default ({ route }: any): JSX.Element => {
   const [data, setData] = useState('HEHEH')
   // useEffect(() => {
